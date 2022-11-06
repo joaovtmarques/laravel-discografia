29a87119e7df3812f0b6d5ac74826582771c5080
João Vitor Marques <jvsilvam@outlook.com>
2022-11-06 18:37:23 -0300

    refactor: rename controllers and main page

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -4,16 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Album extends Controller
+class AlbumController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+      return view('index');
     }
 
     /**
