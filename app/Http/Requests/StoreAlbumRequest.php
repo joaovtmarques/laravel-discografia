ed5e1ae75c5cf84d9d14527852eb1032b8e72cb7
João Vitor Marques <jvsilvam@outlook.com>
2022-11-09 11:44:19 -0300

    feat: add rule to validate store album

--- app/Http/Requests/StoreAlbumRequest.php
+++ app/Http/Requests/StoreAlbumRequest.php
@@ -13,7 +13,7 @@ class StoreAlbumRequest extends FormRequest
 
     public function rules() {
         return [
-          'name'=>'required',
+          'name'=>'required|unique:albums',
           'year'=>'required|digits:4|integer|min:1901|max:'.(date('Y')+1)
         ];
     }
@@ -21,6 +21,7 @@ class StoreAlbumRequest extends FormRequest
     public function messages() {
       return [
         'name.required' => 'O campo nome não pode estar vazio.',
+        'name.unique' => 'Já existe um álbum com este nome.',
         'year.required' => 'O campo ano não pode estar vazio.',
         'year.max' => 'O ano não pode ser maior que 2023.',
       ];
